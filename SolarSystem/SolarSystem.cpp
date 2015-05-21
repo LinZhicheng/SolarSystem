5ad31d4edef06292ad1227dc359d48d8f484c01e
LinZhicheng <linzhicheng1019@live.cn>
2015-05-21 09:54:49 +0800

    贴图、读取图片

--- SolarSystem/SolarSystem.cpp
+++ SolarSystem/SolarSystem.cpp
@@ -1,18 +1,128 @@
 // SolarSystem.cpp : 定义控制台应用程序的入口点。
 //
 
-#include "stdafx.h"
-#include "stdlib.h"
-#include "GL/glut.h"
-#include "GL/glaux.h"
 #include "windows.h"
+#include "GL/glut.h"
+#include "GL/gl.h"
+#include "GL/glu.h"
+#include "GL/glaux.h"
 
 int w = 1024, h = 576;
 int year = 0, month = 0, day = 0;
+GLuint texture[3];
+GLUquadricObj *g_quadratic;
+
+// Load A BMP
+AUX_RGBImageRec *LoadBMP(LPCWSTR Filename)				// Loads A Bitmap Image
+{
+	if (!Filename)										// Make Sure A Filename Was Given
+	{
+		return NULL;									// If Not Return NULL
+	}
+
+	return auxDIBImageLoad(Filename);				// Load The Bitmap And Return A Pointer
+
+}
+
+int LoadGLTextures()									// Load Bitmaps And Convert To Textures
+{
+	int Status = 0;									// Status Indicator
+
+	AUX_RGBImageRec *TextureImage[1];					// Create Storage Space For The Texture
+
+	memset(TextureImage, 0, sizeof(void *) * 1);           	// Set The Pointer To NULL
+
+	// Load The Bitmap, Check For Errors, If Bitmap's Not Found Quit
+	if (TextureImage[0] = LoadBMP(L"资源文件sun256128.bmp"))
+	{
+		Status += 1;
+
+		glGenTextures(1, &texture[0]);					// Create The Texture
+
+		glBindTexture(GL_TEXTURE_2D, texture[0]);
+		gluBuild2DMipmaps(GL_TEXTURE_2D, 3, TextureImage[0]->sizeX, TextureImage[0]->sizeY, GL_RGB, GL_UNSIGNED_BYTE, TextureImage[0]->data);
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_NEAREST);
+		glTexGeni(GL_S, GL_TEXTURE_GEN_MODE, GL_OBJECT_LINEAR);
+		glTexGeni(GL_T, GL_TEXTURE_GEN_MODE, GL_OBJECT_LINEAR);
+		glEnable(GL_TEXTURE_GEN_S);
+		glEnable(GL_TEXTURE_GEN_T);
+	}
+
+	if (TextureImage[0])									// If Texture Exists
+	{
+		if (TextureImage[0]->data)							// If Texture Image Exists
+		{
+			free(TextureImage[0]->data);					// Free The Texture Image Memory
+		}
+
+		free(TextureImage[0]);								// Free The Image Structure
+	}
+
+	memset(TextureImage, 0, sizeof(void *) * 1);           	// Set The Pointer To NULL
+
+	if (TextureImage[0] = LoadBMP(L"资源文件/earth_clouds256128.bmp"))
+	{
+		Status +=1;									
+
+		glGenTextures(1, &texture[1]);					// Create The Texture
+
+		glBindTexture(GL_TEXTURE_2D, texture[1]);
+		gluBuild2DMipmaps(GL_TEXTURE_2D, 3, TextureImage[0]->sizeX, TextureImage[0]->sizeY, GL_RGB, GL_UNSIGNED_BYTE, TextureImage[0]->data);
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_NEAREST);
+		glTexGeni(GL_S, GL_TEXTURE_GEN_MODE, GL_OBJECT_LINEAR);
+		glTexGeni(GL_T, GL_TEXTURE_GEN_MODE, GL_OBJECT_LINEAR);
+		glEnable(GL_TEXTURE_GEN_S);
+		glEnable(GL_TEXTURE_GEN_T);
+	}
+
+	if (TextureImage[0])									// If Texture Exists
+	{
+		if (TextureImage[0]->data)							// If Texture Image Exists
+		{
+			free(TextureImage[0]->data);					// Free The Texture Image Memory
+		}
+
+		free(TextureImage[0]);								// Free The Image Structure
+	}
+
+	memset(TextureImage, 0, sizeof(void *) * 1);           	// Set The Pointer To NULL
+
+	if (TextureImage[0] = LoadBMP(L"资源文件/moon256128.bmp"))
+	{
+		Status += 1;
+
+		glGenTextures(1, &texture[2]);					// Create The Texture
+
+		glBindTexture(GL_TEXTURE_2D, texture[2]);
+		gluBuild2DMipmaps(GL_TEXTURE_2D, 3, TextureImage[0]->sizeX, TextureImage[0]->sizeY, GL_RGB, GL_UNSIGNED_BYTE, TextureImage[0]->data);
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_NEAREST);
+		glTexGeni(GL_S, GL_TEXTURE_GEN_MODE, GL_OBJECT_LINEAR);
+		glTexGeni(GL_T, GL_TEXTURE_GEN_MODE, GL_OBJECT_LINEAR);
+		glEnable(GL_TEXTURE_GEN_S);
+		glEnable(GL_TEXTURE_GEN_T);
+	}
+
+	if (TextureImage[0])									// If Texture Exists
+	{
+		if (TextureImage[0]->data)							// If Texture Image Exists
+		{
+			free(TextureImage[0]->data);					// Free The Texture Image Memory
+		}
+
+		free(TextureImage[0]);								// Free The Image Structure
+	}
+
+	return Status;
+}
 
 void init(GLvoid)     // Create Some Everyday Functions
 {
-
+	int num = LoadGLTextures();
+	if (num != 3)
+		return;
 	glShadeModel(GL_SMOOTH);							// Enable Smooth Shading
 	glClearColor(0.0f, 0.0f, 0.0f, 0.5f);				// Black Background
 	glClearDepth(1.0f);									// Depth Buffer Setup
@@ -20,6 +130,12 @@ void init(GLvoid)     // Create Some Everyday Functions
 	glDepthFunc(GL_LEQUAL);								// The Type Of Depth Testing To Do
 	glEnable(GL_COLOR_MATERIAL);
 	glHint(GL_PERSPECTIVE_CORRECTION_HINT, GL_NICEST);
+
+
+	g_quadratic = gluNewQuadric();
+	if (g_quadratic == 0) return;
+	gluQuadricNormals(g_quadratic, GLU_SMOOTH);        // Create Smooth Normals (NEW)
+	gluQuadricTexture(g_quadratic, GL_TRUE);           // Create Texture Coords (NEW)
 }
 
 void display(void)   // Create The Display Function
@@ -27,31 +143,34 @@ void display(void)   // Create The Display Function
 	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);	// Clear Screen And Depth Buffer
 	glLoadIdentity();									// Reset The Current Modelview Matrix
 	glTranslatef(0.0f, 0.0f, -6.0f);					// Move Left 1.5 Units And Into The Screen 6.0
-	gluLookAt(0, -1, 0, 0, 0, 0, 0, 0, -1);
+	//gluLookAt(0, -1, 0, 0, 0, 0, 0, 0, -1);
 	glColor3f(0.8f, 0.0f, 0.0f);
-	glutSolidSphere(1, 32, 32);
-	
+	glBindTexture(GL_TEXTURE_2D, texture[0]);
+	gluSphere(g_quadratic, 1, 32, 32);
+
 	year = year % 360;
 	month = month % 360;
 	day = day % 360;
-	
+
 	glRotatef((GLfloat)year, 0, 1, 0);
 	glTranslatef(3.0f, 0, 0);
 	glColor3f(0.0f, 0.0f, 1.0f);
-	glutSolidSphere(0.2, 32, 32);
-	
+	glBindTexture(GL_TEXTURE_2D, texture[1]);
+	gluSphere(g_quadratic, 0.2, 32, 32);
+
 	glRotatef((GLfloat)month, 0, 1, 0);
 	glTranslatef(0.5f, 0, 0);
 	glColor3f(1.0f, 1.0f, 1.0f);
-	glutSolidSphere(0.05, 32, 32);
-	
+	glBindTexture(GL_TEXTURE_2D, texture[2]);
+	gluSphere(g_quadratic, 0.05, 32, 32);
+
 	glTranslatef(-0.5f, 0, 0);
 	glTranslatef(-3.0f, 0, 0);
-	
+
 	year += 1;
 	month += 12;
 	glutPostRedisplay();
-	
+
 	Sleep(25);
 
 	glutSwapBuffers();
@@ -63,7 +182,7 @@ void reshape(int w, int h)   // Create The Reshape Function (the viewport)
 	glViewport(0, 0, w, h);
 	glMatrixMode(GL_PROJECTION);  // Select The Projection Matrix
 	glLoadIdentity();                // Reset The Projection Matrix
-	gluPerspective(60, (GLdouble)w/(GLdouble)h, 1.0, 5000.0);
+	gluPerspective(60, (GLdouble)w / (GLdouble)h, 1.0, 5000.0);
 	glMatrixMode(GL_MODELVIEW);  // Select The Model View Matrix
 	glLoadIdentity();    // Reset The Model View Matrix
 }
@@ -86,7 +205,7 @@ void arrow_keys(int a_keys, int x, int y)  // Create Special Function (required 
 		glutFullScreen(); // Go Into Full Screen Mode
 		break;
 	case GLUT_KEY_DOWN:               // When Down Arrow Is Pressed...
-		glutReshapeWindow(640, 480); // Go Into A 500 By 500 Window
+		glutReshapeWindow(w, h); // Go Into A 500 By 500 Window
 		break;
 	default:
 		break;

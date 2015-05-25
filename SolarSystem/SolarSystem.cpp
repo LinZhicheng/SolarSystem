d4c9b58b95c928ed9019d61d37182556193d1c0a
LinZhicheng <linzhicheng1019@live.cn>
2015-05-25 10:26:39 +0800

    除余下两个错误外，贴图仍然未成功，还需调试

--- SolarSystem/SolarSystem.cpp
+++ SolarSystem/SolarSystem.cpp
@@ -1,9 +1,21 @@
-// SolarSystem.cpp : 定义控制台应用程序的入口点。
-//
+/*
+NeHe (nehe.gamedev.net) OpenGL tutorial series
+GLUT port.in 2001 by milix (milix_gr@hotmail.com)
+Most comments are from the original tutorials found in NeHe.
+For VC++ users, create a Win32 Console project and link
+the program with glut32.lib, glu32.lib, opengl32.lib
+*/
 
-#include "windows.h"
-#include "GL/glut.h"
-#include "GL/GLAux.h"
+#include <windows.h>   // Standard Header For MSWindows Applications
+#include <gl/glut.h>   // The GL Utility Toolkit (GLUT) Header
+#include <GL/GLAux.h>
+
+// The Following Directive Fixes The Problem With Extra Console Window
+#pragma comment(linker, "/subsystem:\"windows\" /entry:\"mainCRTStartup\"")
+
+// Global Variables
+bool g_gamemode;       // GLUT GameMode ON/OFF
+bool g_fullscreen;     // Fullscreen Mode ON/OFF (When g_gamemode Is OFF)
 
 int w = 1024, h = 576;
 int year = 0, month = 0, day = 0;
@@ -30,8 +42,8 @@ int LoadGLTextures()									// Load Bitmaps And Convert To Textures
 
 	memset(TextureImage, 0, sizeof(void *) * 1);           	// Set The Pointer To NULL
 
-	// Load The Bitmap, Check For Errors, If Bitmap's Not Found Quit
-	if (TextureImage[0] = LoadBMP(L"资源文件/sun256128.bmp"))
+															// Load The Bitmap, Check For Errors, If Bitmap's Not Found Quit
+	if (TextureImage[0] = LoadBMP(L"sun256128.bmp"))
 	{
 		Status += 1;
 
@@ -59,9 +71,9 @@ int LoadGLTextures()									// Load Bitmaps And Convert To Textures
 
 	memset(TextureImage, 0, sizeof(void *) * 1);           	// Set The Pointer To NULL
 
-	if (TextureImage[0] = LoadBMP(L"资源文件/earth_clouds256128.bmp"))
+	if (TextureImage[0] = LoadBMP(L"earth_clouds256128.bmp"))
 	{
-		Status +=1;									
+		Status += 1;
 
 		glGenTextures(1, &texture[1]);					// Create The Texture
 
@@ -87,7 +99,7 @@ int LoadGLTextures()									// Load Bitmaps And Convert To Textures
 
 	memset(TextureImage, 0, sizeof(void *) * 1);           	// Set The Pointer To NULL
 
-	if (TextureImage[0] = LoadBMP(L"资源文件/moon256128.bmp"))
+	if (TextureImage[0] = LoadBMP(L"moon256128.bmp"))
 	{
 		Status += 1;
 
@@ -116,11 +128,12 @@ int LoadGLTextures()									// Load Bitmaps And Convert To Textures
 	return Status;
 }
 
-void init(GLvoid)     // Create Some Everyday Functions
+// Our GL Specific Initializations
+bool init(void)
 {
 	int num = LoadGLTextures();
 	if (num != 3)
-		return;
+		return false;
 	glShadeModel(GL_SMOOTH);							// Enable Smooth Shading
 	glClearColor(0.0f, 0.0f, 0.0f, 0.5f);				// Black Background
 	glClearDepth(1.0f);									// Depth Buffer Setup
@@ -131,17 +144,20 @@ void init(GLvoid)     // Create Some Everyday Functions
 
 
 	g_quadratic = gluNewQuadric();
-	if (g_quadratic == 0) return;
+	if (g_quadratic == 0) return false;
 	gluQuadricNormals(g_quadratic, GLU_SMOOTH);        // Create Smooth Normals (NEW)
 	gluQuadricTexture(g_quadratic, GL_TRUE);           // Create Texture Coords (NEW)
+	return true;
 }
 
-void display(void)   // Create The Display Function
+// Our Rendering Is Done Here
+void render(void)
 {
 	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);	// Clear Screen And Depth Buffer
 	glLoadIdentity();									// Reset The Current Modelview Matrix
+
 	glTranslatef(0.0f, 0.0f, -6.0f);					// Move Left 1.5 Units And Into The Screen 6.0
-	//gluLookAt(0, -1, 0, 0, 0, 0, 0, 0, -1);
+														//gluLookAt(0, -1, 0, 0, 0, 0, 0, 0, -1);
 	glColor3f(0.8f, 0.0f, 0.0f);
 	glBindTexture(GL_TEXTURE_2D, texture[0]);
 	gluSphere(g_quadratic, 1, 32, 32);
@@ -171,56 +187,85 @@ void display(void)   // Create The Display Function
 
 	Sleep(25);
 
+	// Swap The Buffers To Become Our Rendering Visible
 	glutSwapBuffers();
-	// Swap The Buffers To Not Be Left With A Clear Screen
 }
 
-void reshape(int w, int h)   // Create The Reshape Function (the viewport)
+// Our Reshaping Handler (Required Even In Fullscreen-Only Modes)
+void reshape(int w, int h)
 {
 	glViewport(0, 0, w, h);
-	glMatrixMode(GL_PROJECTION);  // Select The Projection Matrix
+	glMatrixMode(GL_PROJECTION);     // Select The Projection Matrix
 	glLoadIdentity();                // Reset The Projection Matrix
-	gluPerspective(60, (GLdouble)w / (GLdouble)h, 1.0, 5000.0);
-	glMatrixMode(GL_MODELVIEW);  // Select The Model View Matrix
-	glLoadIdentity();    // Reset The Model View Matrix
+									 // Calculate The Aspect Ratio And Set The Clipping Volume
+	if (h == 0) h = 1;
+	gluPerspective(80, (float)w / (float)h, 1.0, 5000.0);
+	glMatrixMode(GL_MODELVIEW);      // Select The Modelview Matrix
+	glLoadIdentity();                // Reset The Modelview Matrix
 }
 
-void keyboard(unsigned char key, int x, int y)  // Create Keyboard Function
+// Our Keyboard Handler (Normal Keys)
+void keyboard(unsigned char key, int x, int y)
 {
 	switch (key) {
 	case 27:        // When Escape Is Pressed...
-		exit(0);   // Exit The Program
-		break;        // Ready For Next Case
+		exit(0);    // Exit The Program
+		break;          // Ready For Next Case
 	default:        // Now Wrap It Up
 		break;
 	}
 }
 
-void arrow_keys(int a_keys, int x, int y)  // Create Special Function (required for arrow keys)
+// Our Keyboard Handler For Special Keys (Like Arrow Keys And Function Keys)
+void special_keys(int a_keys, int x, int y)
 {
 	switch (a_keys) {
-	case GLUT_KEY_UP:     // When Up Arrow Is Pressed...
-		glutFullScreen(); // Go Into Full Screen Mode
-		break;
-	case GLUT_KEY_DOWN:               // When Down Arrow Is Pressed...
-		glutReshapeWindow(w, h); // Go Into A 500 By 500 Window
+	case GLUT_KEY_F1:
+		// We Can Switch Between Windowed Mode And Fullscreen Mode Only
+		if (!g_gamemode) {
+			g_fullscreen = !g_fullscreen;       // Toggle g_fullscreen Flag
+			if (g_fullscreen) glutFullScreen(); // We Went In Fullscreen Mode
+			else glutReshapeWindow(w, h);   // We Went In Windowed Mode
+		}
 		break;
 	default:
 		break;
 	}
 }
 
-void main(int argc, char** argv)   // Create Main Function For Bringing It All Together
+// Ask The User If He Wish To Enter GameMode Or Not
+void ask_gamemode()
 {
-	glutInit(&argc, argv); // Erm Just Write It =)
-	init();
-	glutInitDisplayMode(GLUT_RGB | GLUT_DOUBLE); // Display Mode
-	glutInitWindowSize(w, h); // If glutFullScreen wasn't called this is the window size
-	glutCreateWindow("Solar System"); // Window Title (argv[0] for current directory as title)
-	glutDisplayFunc(display);  // Matching Earlier Functions To Their Counterparts
-	glutReshapeFunc(reshape);
-	glutKeyboardFunc(keyboard);
-	glutSpecialFunc(arrow_keys);
-	glutMainLoop();          // Initialize The Main Loop
+	int answer;
+	// Use Windows MessageBox To Ask The User For Game Or Windowed Mode
+	answer = MessageBox(NULL, L"Do you want to enter game mode?", L"Question",
+		MB_ICONQUESTION | MB_YESNO);
+	g_gamemode = (answer == IDYES);
+	// If Not Game Mode Selected, Use Windowed Mode (User Can Change That With F1)
+	g_fullscreen = false;
 }
 
+// Main Function For Bringing It All Together.
+int main(int argc, char** argv)
+{
+	ask_gamemode();                                  // Ask For Fullscreen Mode
+	glutInit(&argc, argv);                           // GLUT Initializtion
+	glutInitDisplayMode(GLUT_RGB | GLUT_DOUBLE);     // Display Mode (Rgb And Double Buffered)
+	if (g_gamemode) {
+		glutGameModeString("640x480:16");            // Select The 640x480 In 16bpp Mode
+		if (glutGameModeGet(GLUT_GAME_MODE_POSSIBLE))
+			glutEnterGameMode();                     // Enter Full Screen
+		else g_gamemode = false;                     // Cannot Enter Game Mode, Switch To Windowed
+	}
+	if (!g_gamemode) {
+		glutInitWindowSize(w, h);                // Window Size If We Start In Windowed Mode
+		glutCreateWindow("SolarSystem"); // Window Title 
+	}
+	init();                                          // Our Initialization
+	glutDisplayFunc(render);                         // Register The Display Function
+	glutReshapeFunc(reshape);                        // Register The Reshape Handler
+	glutKeyboardFunc(keyboard);                      // Register The Keyboard Handler
+	glutSpecialFunc(special_keys);                   // Register Special Keys Handler
+	glutMainLoop();                                  // Go To GLUT Main Loop
+	return 0;
+}
